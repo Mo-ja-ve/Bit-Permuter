6c2a89bce9a7c3c4637598d305dff3c4d0b16c44
Mo-ja-ve <ak461717@ohio.edu>
2022-09-19 19:43:24 -0400

    p8()

--- main.cpp
+++ main.cpp
@@ -12,18 +12,39 @@ unsigned short p10(unsigned short p){
 	for(int i = 1; i <= 10; i++){
 		n = (p << (16 - perm_scheme[i-1]));
 		n = (n >> 15);
-		n =  n << 10 - i;
+		n = (n << 10 - i);
 		permuted = permuted | n; 
 	}
 
 	return permuted;
 }
 
+unsigned short p8(unsigned short p){
+	
+	unsigned short mask = 0;
+	unsigned short L = 0;// Left half 5 bits
+	unsigned short R = 0;// Right half 5 bits
+
+	L = (p >> 5);
+	R = (p << 11);
+	R = (R >> 11);
+	
+	mask = (L << 12);
+	mask = (mask >> 11);
+	L = (L >> 4);
+	L = L | mask;
+
+
+	
+}
+
 int main() {
 
 	unsigned short p = 682;
-	//p10(p);
-	cout<< endl<< "PERMUTED 10 BITS: "<< p10(p);
+	p = p10(p);
+	//cout<< endl<< "PERMUTED 10 BITS: "<< p10(p);
+
+	cout<<endl<<"CIRCLE LEFT SHIFT 1: "<< p8(p);
 
 	return 0;
 }
